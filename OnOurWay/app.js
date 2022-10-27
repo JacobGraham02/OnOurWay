355c496b19045636286e71b127b1a723ee83f85f
Owen <oalaurie@lakeheadu.ca>
2022-10-27 09:28:59 -0400

    Layout works, index exists but 404's :)))))

--- OnOurWay/app.js
+++ OnOurWay/app.js
@@ -9,6 +9,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.get('/', function(request, response) {
+  response.send();
+});
+
+app.listen(3003, function() {
+  console.log("This application is listening to port 3000");
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
