d84deefa0fa1b5ae4d99566ffad1071012f7ae78
Jacob Graham <jakegraham54@gmail.com>
2022-10-27 23:19:26 -0400

    Modified all variable names in the project .env file. Updated the mysql2 npm package version in both package.json and package-lock.json. Added a starting DAO template structure in CustomerDAO

--- OnOurWay/app.js
+++ OnOurWay/app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('dotenv').config()
+var mysql = require('mysql2');
+require('dotenv').config({path:'\.env'});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
