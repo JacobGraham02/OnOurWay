61579becefd26a7f9e2d81d68c4c6676c253be43
Jacob Graham <jakegraham54@gmail.com>
2022-10-27 23:18:52 -0400

    Modified all variable names in the project .env file. Updated the mysql2 npm package version in both package.json and package-lock.json. Added a starting DAO template structure in CustomerDAO

--- OnOurWay/persistence/CustomerDAO.js
+++ OnOurWay/persistence/CustomerDAO.js
@@ -1,4 +1,26 @@
 var database_manager = require('./DatabaseConnectionManager');
 var mysql = require('mysql2');
 
+const database_connection = database_manager.getDatabaseConnection;
 
+
+const getAllCustomers = function() {
+
+}
+
+const addCustomer = function() {
+
+}
+
+const updateCustomer = function() {
+
+}
+
+const deleteCustomer = function() {
+
+}
+
+exports.getAllCustomers = getAllCustomers();
+exports.addCustomer = addCustomer();
+exports.updateCustomer = updateCustomer();
+exports.deleteCustomer = deleteCustomer();
