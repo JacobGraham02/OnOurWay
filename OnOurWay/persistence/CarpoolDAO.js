dfc9eae3021ac8af784722bf56d2aa81820c7a64
Jacob Graham <jakegraham54@gmail.com>
2022-11-17 08:35:28 -0500

    Modified persistence/CarpoolDAO.js to retrieve a mysql connection from a prebuilt pool of connections. Added an index route to routes/carpool.js to view list of all available carpools. Added a view file for the detail carpool route (views/carpool/detailed_routes.pug) and added a view file for viewing an overview of all available carpools

--- OnOurWay/persistence/CarpoolDAO.js
+++ OnOurWay/persistence/CarpoolDAO.js
@@ -4,7 +4,7 @@ var mysql = require('mysql2');
 const getAllFromCarpool = function() {
     const query_string = `SELECT * FROM Carpool`;
     return new Promise(function(resolve, reject) {
-        database_manager.initialize_database_connection_pool.getConnection(function(error, connection) {
+        database_manager.initialize_database_connection_pool().getConnection(function(error, connection) {
             connection.query(query_string, function(error, results) {
                 if (error) {
                     reject(error);
@@ -20,7 +20,7 @@ const getSpecificCarpool= function(where) {
     const where_clause = concatenateSqlQueryStringForWhereClause(where);
     let query_string = `SELECT * FROM Carpool WHERE ${where_clause} LIMIT 1000`;
     return new Promise(function(resolve, reject) {
-        database_manager.initialize_database_connection_pool.getConnection(function(error, connection) {
+        database_manager.initialize_database_connection_pool().getConnection(function(error, connection) {
             connection.query(query_string, function(error, results) {
                 if (error) {
                     reject(error);
@@ -36,7 +36,7 @@ const addCarpool = function(customer) {
     let query_string = `INSERT INTO CUSTOMER (first_name, last_name, credit_card_number, credit_card_cvc, credit_card_effective_date, credit_card_expiry_date, phone_number, 
         email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
     return new Promise(function(resolve, reject) {
-        database_manager.initialize_database_connection_pool.getConnection(function(error, connection) {
+        database_manager.initialize_database_connection_pool().getConnection(function(error, connection) {
             connection.query(query_string, [customer.first_name, customer.last_name, customer.credit_card_number, customer.credit_card_cvc,
                 customer.credit_card_effective_date, customer.credit_card_expiry_date, customer.phone_number, customer.email], function(error, results) {
                 if (error) {
@@ -54,7 +54,7 @@ const updateCarpool = function(query_data) {
     const where_clause_string = concatenateSqlQueryStringForWhereClause(query_data.where_clause);
     let query_string = `UPDATE Carpool SET ${column_and_values_sql_string} WHERE ${where_clause_string}`;
     return new Promise(function(resolve, reject) {
-        database_manager.initialize_database_connection_pool.getConnection(function(error, connection) {
+        database_manager.initialize_database_connection_pool().getConnection(function(error, connection) {
             connection.query(query_string, function(error, results) {
                 if (error) {
                     reject(error);
@@ -70,7 +70,7 @@ const deleteCarpool = function(where) {
     const where_clause_string = concatenateSqlQueryStringForWhereClause(where);  
     let query_string = `DELETE FROM Carpool WHERE ${where_clause_string}`;
     return new Promise(function(resolve, reject) {
-        database_manager.initialize_database_connection_pool.getConnection(function(error, connection) {
+        database_manager.initialize_database_connection_pool().getConnection(function(error, connection) {
             connection.query(query_string, function(error, results) {
                 if (error) {
                     reject(error);
@@ -113,37 +113,3 @@ exports.getSpecificCarpool = getSpecificCarpool;
 exports.addCarpool = addCarpool;
 exports.updateCarpool = updateCarpool;
 exports.deleteCarpool = deleteCarpool;
-
-/*
-const customer = {
-    first_name: "Jacob",
-    last_name: "Graham",
-    credit_card_number: 1111,
-    credit_card_cvc: 111,
-    credit_card_effective_date: '2022-10-28',
-    credit_card_expiry_date: '2022-10-28',
-    phone_number: 7777,
-    email: "test5@gmail.com"
-};
-const query_info = {
-    table_name: "Customer",
-    total_columns: 2,
-    column_names: [
-        'first_name',
-        'last_name'
-    ],
-    column_values: [
-        'SecondFirstName',
-        'ThirdFirstName'
-    ],
-    where_clause: [
-        'Customer.first_name = "SecondFirstName"'
-    ],
-};
-
-INSERT INTO Customer (first_name, last_name, credit_card_number, credit_card_cvc, credit_card_effective_date, credit_card_expiry_date, phone_number, email)
-getAllFromCustomerTable().then((results) => console.log(results));
-addCustomer(customer).then((results) => console.log(results));
-getSpecificCustomerFromCustomerTable(query_info.where_clause).then((results) => console.log(results));
-deleteCustomer(query_info.where_clause).then((results) => console.log(results));
-*/
