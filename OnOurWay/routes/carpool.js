37dbb8e6e8666e4871b44819c1943b2489ee77ea
Jacob Graham <jakegraham54@gmail.com>
2022-11-19 16:37:50 -0500

    Deleted unnecessary 'route' files. Added user authentication to routes/carpool.js so that only users who are logged in can access certain site locations

--- OnOurWay/routes/carpool.js
+++ OnOurWay/routes/carpool.js
@@ -2,15 +2,23 @@ var express = require('express');
 var router = express.Router();
 const carpoolDAO = require('../persistence/CarpoolDAO');
 
+function isLoggedIn(request, response, next) {
+  if (request.isAuthenticated()) {
+    return next();
+  } else {
+    response.redirect('/login');
+  }
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', isLoggedIn, function(req, res, next) {
   const carpool_locations = carpoolDAO.getAllFromCarpool();
   carpool_locations.then((results) => {
     res.render('carpool/index', {carpool_locations: results});
   });
 });
 
-router.get('/detailed_route', function(request, response, next) {
+router.get('/detailed_route', isLoggedIn, function(request, response, next) {
   const request_parameter_id = request.query.id;
   const query_where_clause = `id = ${request_parameter_id}`;
   const carpool_result = carpoolDAO.getSpecificCarpool(query_where_clause);
